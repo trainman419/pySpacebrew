d964af54d9125b3631ea3449fe0ecbca5332153b
trainman419 <legotown@aol.com>
2013-12-31 14:22:26 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+setup(name='pySpacebrew', version='0.1.0',
+      description="Python library for Spacebrew",
+      packages=['pySpacebrew'])
